8f9040fd7ce220c01fe1a31d4629db15ad966f76
eunhee Ahn <4off4@naver.com>
2023-02-09 14:56:49 +0900

    [20230209] #3.6 Answer

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Noom 
 Zoom Clone using NodeJS, WebRTC and Websockets. <br/>
 <div>
-    <h3>SocketIO</h3>
+    <h3>git push https://github.com/4off4/VIDEOCALLNOOM.git main -f</h3>
 </div>
--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,12 +1,18 @@
 const socket = io();
+
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
+const call  = document.getElementById("call");
+
+call.hidden = true;
 
 let myStream; 
 let muted = false;
 let cameraOff = false;
+let roomName;
+let myPeerConnection; 
 
 async function getCameras() {
     try{
@@ -53,8 +59,6 @@ async function getMedia(deviceId) {
     }
 }
 
-getMedia();
-
 function handleMuteClick() {
     myStream.getAudioTracks().forEach((track) => (track.enabled = !track.enabled));
 
@@ -85,3 +89,57 @@ async function handleCameraChange() {
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
 camerasSelect.addEventListener("input", handleCameraChange);
+
+
+// Welcome form (방 선택)
+const welcome = document.getElementById("welcome"); 
+const welcomeForm = welcome.querySelector("form");
+
+async function initCall() {
+    welcome.hidden = true;
+    call.hidden = false;
+    await getMedia();
+    makeConnection();
+}
+
+async function handleWelcomeSubmit(event){
+    event.preventDefault();
+    const input = welcomeForm.querySelector("input");
+    await initCall();
+
+    socket.emit("join_room", input.value.toString('utf8'));
+    roomName = input.value.toString('utf8');
+    input.value = "";
+}
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+
+/* Socekt 코드 */ 
+
+// 해당 코드는 peer A에서 실행 
+socket.on("welcome", async () => {
+    const offer = await myPeerConnection.createOffer();
+    myPeerConnection.setLocalDescription(offer);
+    console.log("Sent the offer");
+    socket.emit("offer", offer, roomName);
+});
+
+// 해당 코드는 peer B에서 실행 
+socket.on("offer", async(offer) => {
+    myPeerConnection.setRemoteDescription(offer);
+    const answer = await myPeerConnection.createAnswer();
+    myPeerConnection.setLocalDescription(answer);
+    socket.emit("answer", answer, roomName);
+});
+
+socket.on("answer", (answer) => {
+    myPeerConnection.setRemoteDescription(answer);
+});
+
+// RTC 코드 
+function makeConnection(){                     
+    myPeerConnection = new RTCPeerConnection();
+    // peer-to-peer 연결 
+    myStream.getTracks().forEach((track) => myPeerConnection.addTrack(track, myStream));
+    // addTrack: track들을 개별적으로 추가해주는 함수 
+}
